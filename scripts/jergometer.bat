d1c6fe56abda7986a52724aea09c491855be493b
Stefan Endrullis <stefan@endrullis.de>
2009-12-19 18:14:37 +0000

    on the way to an automatic jar/zip/tar.gz generation

--- scripts/jergometer.bat
+++ scripts/jergometer.bat
@@ -6,6 +6,6 @@ move tmp\* .
 rmdir tmp
 
 :START
-java -jar JLatexEditor.jar %1
+java -Djava.library.path=lib/dlls/Windows/i368 -cp jergometer.jar;RXTXcomm.jar org.jergometer.Jergometer %1
 
 IF errorlevel 255 goto UPDATE
