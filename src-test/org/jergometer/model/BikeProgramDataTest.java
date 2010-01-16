28565851c0d8853b817fccea8851a011cd75f028
Stefan Endrullis <stefan@endrullis.de>
2010-01-16 10:34:06 +0000

    implemented feature #164: allow relative values (with prefix "+") in training programs (for time, pulse and power)

--- src-test/org/jergometer/model/BikeProgramDataTest.java
+++ src-test/org/jergometer/model/BikeProgramDataTest.java
@@ -7,21 +7,21 @@ import junit.framework.TestCase;
  */
 public class BikeProgramDataTest extends TestCase {
 	public void testTimeDefinitions() {
-		assertEquals(60, BikeProgramData.getTime("60"));
-		assertEquals(60, BikeProgramData.getTime("60s"));
-		assertEquals(60, BikeProgramData.getTime("1m"));
-		assertEquals(60, BikeProgramData.getTime("1'"));
-		assertEquals(60, BikeProgramData.getTime("1:0"));
-		assertEquals(60, BikeProgramData.getTime("1:00"));
-		assertEquals(60, BikeProgramData.getTime("1.0m"));
-		assertEquals(60, BikeProgramData.getTime("1,0m"));
-		assertEquals(90, BikeProgramData.getTime("1,5m"));
-		assertEquals(90, BikeProgramData.getTime("1m30"));
-		assertEquals(90, BikeProgramData.getTime("1m30s"));
-		assertEquals(90, BikeProgramData.getTime("01:30"));
-		assertEquals(90, BikeProgramData.getTime("0:01:30"));
-		assertEquals(90, BikeProgramData.getTime("0h01m30"));
-		assertEquals(3661, BikeProgramData.getTime("1:1:1"));
-		assertEquals(3661, BikeProgramData.getTime("1h1m1"));
+		assertEquals(60, BikeProgramData.GetTime.inst.apply("60"));
+		assertEquals(60, BikeProgramData.GetTime.inst.apply("60s"));
+		assertEquals(60, BikeProgramData.GetTime.inst.apply("1m"));
+		assertEquals(60, BikeProgramData.GetTime.inst.apply("1'"));
+		assertEquals(60, BikeProgramData.GetTime.inst.apply("1:0"));
+		assertEquals(60, BikeProgramData.GetTime.inst.apply("1:00"));
+		assertEquals(60, BikeProgramData.GetTime.inst.apply("1.0m"));
+		assertEquals(60, BikeProgramData.GetTime.inst.apply("1,0m"));
+		assertEquals(90, BikeProgramData.GetTime.inst.apply("1,5m"));
+		assertEquals(90, BikeProgramData.GetTime.inst.apply("1m30"));
+		assertEquals(90, BikeProgramData.GetTime.inst.apply("1m30s"));
+		assertEquals(90, BikeProgramData.GetTime.inst.apply("01:30"));
+		assertEquals(90, BikeProgramData.GetTime.inst.apply("0:01:30"));
+		assertEquals(90, BikeProgramData.GetTime.inst.apply("0h01m30"));
+		assertEquals(3661, BikeProgramData.GetTime.inst.apply("1:1:1"));
+		assertEquals(3661, BikeProgramData.GetTime.inst.apply("1h1m1"));
 	}
 }
--- src/org/jergometer/model/BikeProgramData.java
+++ src/org/jergometer/model/BikeProgramData.java
@@ -20,13 +20,15 @@ public class BikeProgramData {
 
 	public void fromXML(XMLElement rootElement) {
 		name = rootElement.getAttribute("name");
-		duration = getTime(rootElement.getAttribute("duration"));
+		duration = getValue(GetTime.inst, rootElement.getAttribute("duration"), 0);
+
+		LastState lastState = new LastState();
 
 		XMLElement timeEventsElement = rootElement.getChildElement("timeevents");
 		if(timeEventsElement != null) {
 			for (XMLElement timeEventElement : timeEventsElement.getChildElements()) {
 				if (timeEventElement.getName().equals("timeevent")) {
-					events.add(new TimeEvent(timeEventElement));
+					events.add(new TimeEvent(timeEventElement, lastState));
 				}
 			}
 		}
@@ -50,74 +52,139 @@ public class BikeProgramData {
 		return events;
 	}
 
-	public static int getTime(String timeString) {
-		timeString = timeString.replaceAll(",", ".");
-		String h = null, m = null, s = null;
-		if (timeString.indexOf(':') >= 0) {
-			String[] parts = timeString.split(":");
-			switch (parts.length) {
-				case 3:
-					h = parts[0];
-					m = parts[1];
-					s = parts[2];
-					break;
-					case 2:
-						m = parts[0];
-						s = parts[1];
-						break;
-					case 1:
-						s = parts[0];
-			}
+
+
+	private static int getValue(IntFunction1<String> f, String string, int lastValue) {
+		int sign = 0;
+
+		if (string.startsWith("+")) {
+			sign = 1;
+			string = string.substring(1);
+		} else
+		if (string.startsWith("-")) {
+			sign = -1;
+			string = string.substring(1);
+		}
+
+		if (sign == 0) {
+			return f.apply(string);
 		} else {
-			if (timeString.indexOf('h') >= 0) {
-				String[] parts = timeString.split("h");
-				h = parts[0];
+			return lastValue + sign * f.apply(string);
+		}
+	}
+	
+
+
+	public static interface IntFunction1<T> {
+		public int apply(String s);
+	}
+
+	public static class GetTime implements IntFunction1<String> {
+		public static final GetTime inst = new GetTime();
+
+		public int apply(String timeString) {
+			timeString = timeString.replaceAll(",", ".");
+			String h = null, m = null, s = null;
+			if (timeString.indexOf(':') >= 0) {
+				String[] parts = timeString.split(":");
+				switch (parts.length) {
+					case 3:
+						h = parts[0];
+						m = parts[1];
+						s = parts[2];
+						break;
+						case 2:
+							m = parts[0];
+							s = parts[1];
+							break;
+						case 1:
+							s = parts[0];
+				}
+			} else {
+				if (timeString.indexOf('h') >= 0) {
+					String[] parts = timeString.split("h");
+					h = parts[0];
+						timeString = parts.length == 2 ? parts[1] : "0";
+				}
+				if (timeString.indexOf('m') >= 0) {
+					String[] parts = timeString.split("m");
+					m = parts[0];
 					timeString = parts.length == 2 ? parts[1] : "0";
+				}
+				if (timeString.indexOf('\'') >= 0) {
+					String[] parts = timeString.split("'");
+					m = parts[0];
+					timeString = parts.length == 2 ? parts[1] : "0";
+				}
+				s = timeString.replace("s", "");
 			}
-			if (timeString.indexOf('m') >= 0) {
-				String[] parts = timeString.split("m");
-				m = parts[0];
-				timeString = parts.length == 2 ? parts[1] : "0";
-			}
-			if (timeString.indexOf('\'') >= 0) {
-				String[] parts = timeString.split("'");
-				m = parts[0];
-				timeString = parts.length == 2 ? parts[1] : "0";
-			}
-			s = timeString.replace("s", "");
-		}
 
-		int time = 0;
-		if (h != null) {
-			time += (int) (3600 * Double.parseDouble(h));
-		}
-		if (m != null) {
-			time += (int) (60 * Double.parseDouble(m));
-		}
-		if (s != null) {
-			time += Integer.parseInt(s);
-		}
+			int time = 0;
+			if (h != null) {
+				time += (int) (3600 * Double.parseDouble(h));
+			}
+			if (m != null) {
+				time += (int) (60 * Double.parseDouble(m));
+			}
+			if (s != null) {
+				time += Integer.parseInt(s);
+			}
 
-		return time;
+			return time;
+		}
+	}
+	public static class GetInt implements IntFunction1<String> {
+		public static final GetInt inst = new GetInt();
+
+		public int apply(String s) {
+			return Integer.parseInt(s);
+		}
 	}
 
 
+
 	// inner classes
+	private class LastState {
+		int time = 0;
+		int power = 0;
+		int pulse = 0;
+
+		public int getLast(Action.Type type) {
+			switch (type) {
+				case power:
+					return power;
+				case pulse:
+					return pulse;
+			}
+			return -1;
+		}
+
+		public void setLast(Action.Type type, int value) {
+			switch (type) {
+				case power:
+					power = value;
+					return;
+				case pulse:
+					pulse = value;
+			}
+		}
+	}
+
 	public static class TimeEvent {
 		protected int time;
 		protected ArrayList<Action> actions = new ArrayList<Action>();
 
-		public TimeEvent(XMLElement element) {
-			fromXML(element);
+		public TimeEvent(XMLElement element, LastState lastState) {
+			fromXML(element, lastState);
 		}
 
-		public void fromXML(XMLElement element) {
-			String timeString = element.getAttribute("time");
-			time = BikeProgramData.getTime(timeString);
+		public void fromXML(XMLElement element, LastState lastState) {
+			time = BikeProgramData.getValue(GetTime.inst, element.getAttribute("time"), lastState.time);
+			lastState.time = time;
 
 			for (XMLElement actionElement : element.getChildElements()) {
 				if(actionElement.getName().equals("action")) {
-					actions.add(new Action(actionElement));
+					actions.add(new Action(actionElement, lastState));
 				}
 			}
 		}
@@ -143,13 +210,14 @@ public class BikeProgramData {
 			this.value = value;
 		}
 
-		public Action(XMLElement element) {
-			fromXML(element);
+		public Action(XMLElement element, LastState lastState) {
+			fromXML(element, lastState);
 		}
 
-		public void fromXML(XMLElement element) {
+		public void fromXML(XMLElement element, LastState lastState) {
 			type = Type.valueOf(element.getAttribute("type"));
-			value = Integer.parseInt(element.getAttribute("value"));
+			value = BikeProgramData.getValue(GetInt.inst, element.getAttribute("value"), lastState.getLast(type));
+			lastState.setLast(type, value);
 		}
 
 		// getters
